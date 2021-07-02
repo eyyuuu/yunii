00fb8cc17a61149e336a6a653a8c5a5d12068f39
Tomjerry12345 <Tomjerry12345@gmail.com>
2021-07-02 22:50:21 +0800

    penambahan recyclerview

--- app/src/main/java/com/example/myapplication/LoginActivity.kt
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -2,11 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val edtEmail = findViewById<EditText>(R.id.edtEmail)
+        val btnEnter = findViewById<Button>(R.id.btnEnter)
+
+        btnEnter.setOnClickListener {
+            Toast.makeText(this, "Succes", Toast.LENGTH_SHORT).show()
+        }
     }
 }
