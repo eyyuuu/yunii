26fca74f35fa86f296a95bf731d56d2b7126b21c
ASUS <Andrialfiandi46@gmail.com>
2021-07-02 18:59:24 +0800

    tambah tampilan

--- app/src/main/java/com/example/myapplication/LokasiActivity.kt
+++ app/src/main/java/com/example/myapplication/LokasiActivity.kt
@@ -3,11 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class LokasiActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        setContentView(R.layout.activity_lokasi)
     }
 }
-
