eaca0184393280ca00618dd5cf50cc9e9bf8beb7
ASUS <Andrialfiandi46@gmail.com>
2021-07-06 20:58:41 +0800

    06-07-2021

--- app/src/main/java/com/example/myapplication/PilihBarangActivity.kt
+++ app/src/main/java/com/example/myapplication/PilihBarangActivity.kt
@@ -8,5 +8,7 @@ class PilihBarangActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pilih_barang)
+
+
     }
 }
